624934d60f6101044db1023d0092fcbd746db4bf
rajashekar reddy <chrajashekarreddy777@gmail.com>
2021-12-29 15:13:46 +0530

    conversion of integer to double using functional interface

--- src/com/bl/lamda/NumberList.java
+++ src/com/bl/lamda/NumberList.java
@@ -4,17 +4,18 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class NumberList {
 	public static void main(String[] args) {
-		List<Integer> number = new ArrayList<>();
-		 
+		List<Integer> intList = new ArrayList<>();
+
 		for (int i = 0; i < 5; i ++) {
-			number.add((int)(Math.random() * 100));
+			intList.add((int)(Math.random() * 100));
 		}
-		
-		Iterator<Integer> itrNumbers = number.iterator();
-		
+
+		Iterator<Integer> itrNumbers = intList.iterator();
+
 		System.out.println("Method1 : iterate each element of the list with Itorater");
 		while(itrNumbers.hasNext()) {
 			System.out.println("Iterator Value :"+itrNumbers.next());
@@ -23,10 +24,30 @@ public class NumberList {
 		Consumer<Integer> intConsumer = n -> {
 			System.out.println("for each number :" +n);
 		};
-		number.forEach(intConsumer);
-		
+		intList.forEach(intConsumer);
+
 		System.out.println("Method3 : Iterate each element of list with MyConsumer");
 		MyConsumer  consumer = new MyConsumer();
-		number.forEach(consumer);
-		}
+		intList.forEach(consumer);
+
+		System.out.println("Method4 : Iterate each element of list with new Consumer");
+		intList.forEach(new Consumer<Integer>() {
+			@Override
+			public void accept(Integer n) {
+				System.out.println("Math4 new Consumer : " + n);
+			}
+		});
+
+		System.out.println("Method5 : Iterate each element of list with Lambda");
+		intList.forEach( n -> {
+			System.out.println("math5 lambda :"+ n);
+		});
+		
+		Function<Integer, Double> toDoubleValue = Integer :: doubleValue;
+		List<Double> doubleList = new ArrayList<>();
+		intList.forEach(n ->{
+			doubleList.add(toDoubleValue.apply(n));
+		});
+		System.out.println("Method6 double Values : "+doubleList);
+	}
 }

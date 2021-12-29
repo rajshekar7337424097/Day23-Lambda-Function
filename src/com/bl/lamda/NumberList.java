2971a1b781d4a76097ac4917dd6c5ce4be1a6a44
rajashekar reddy <chrajashekarreddy777@gmail.com>
2021-12-29 14:04:24 +0530

    performing the iteration operation using Consumer, Lambda

--- src/com/bl/lamda/NumberList.java
+++ src/com/bl/lamda/NumberList.java
@@ -15,16 +15,18 @@ public class NumberList {
 		
 		Iterator<Integer> itrNumbers = number.iterator();
 		
+		System.out.println("Method1 : iterate each element of the list with Itorater");
 		while(itrNumbers.hasNext()) {
 			System.out.println("Iterator Value :"+itrNumbers.next());
 		}
-		
+		System.out.println("Method2 : Iterate each element of the list with Consumer");
 		Consumer<Integer> intConsumer = n -> {
 			System.out.println("for each number :" +n);
 		};
-		
 		number.forEach(intConsumer);
 		
-	}
-
+		System.out.println("Method3 : Iterate each element of list with MyConsumer");
+		MyConsumer  consumer = new MyConsumer();
+		number.forEach(consumer);
+		}
 }
